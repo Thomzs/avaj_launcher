f7517669fedae807f325052b50dbc3d5b992a19c
tmeyer <tmeyer24@student.42.fr>
2022-02-12 00:48:28 +0100

    Adding run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-find * -name '*.java' > .sources &&
+find src -name '*.java' > .sources &&
 javac -cp src @.sources -d .build/ &&
 java -cp .build edu.tmeyer.avaj_launcher.Main "$1" &&
 rm -rf .build
